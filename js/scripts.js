a49f09db88e16f28d29e3f55b3341afb5fa5880e
Osasere Ikponmwosa <edenosas1@gmail.com>
2025-09-13 05:32:14 +0100

    final

--- js/scripts.js
+++ js/scripts.js
@@ -75,3 +75,8 @@ setInterval(() => {
 }, 5000);
 
 
+function togglePassword(inputId) {
+    const input = document.getElementById(inputId);
+    const type = input.getAttribute('type') === 'password' ? 'text' : 'password';
+    input.setAttribute('type', type);
+}
